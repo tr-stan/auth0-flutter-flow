dcd973d1e0586babe927b9e282b8c5be4d2ce4f2
Tristan Bennett <betriistan@gmail.com>
2020-09-01 23:25:53 -0500

    add login widget to show when users not logged in

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,29 @@ class Profile extends StatelessWidget {
 ///            Login Widget
 /// -----------------------------------
 
+class Login extends StatelessWidget {
+  final loginAction;
+  final String loginError;
+
+  const Login(this.loginAction, this.loginError);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        RaisedButton(
+          onPressed: () {
+            loginAction();
+          },
+          child: Text('Login'),
+        ),
+        Text(loginError ?? ''),
+      ],
+    );
+  }
+}
+
 /// -----------------------------------
 ///                 App
 /// -----------------------------------
